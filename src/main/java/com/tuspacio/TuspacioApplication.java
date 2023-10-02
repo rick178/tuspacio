f34a0ae490279160cc32eb610a633653cc9b9cf8
rick <rick178@hotmail.com>
2023-10-01 18:50:32 -0600

    Begining tuspacio proyect

---
+++ src/main/java/com/tuspacio/TuspacioApplication.java
@@ -0,0 +1,13 @@
+package com.tuspacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuspacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuspacioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuspacio/TuspacioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuspacio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuspacioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
